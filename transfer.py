1c7398c38d5440b9003afbfe8bb41ea586ab4994
boltzkreig <115170807+boltzkreig@users.noreply.github.com>
2023-10-28 21:25:22 +0530

    Add files via upload

---
+++ transfer.py
@@ -0,0 +1,26 @@
+#! /bin/python3
+
+################################################################################
+# A Python script to open a http(1.1) server INSECURELY for file transfer in LAN
+################################################################################
+
+import socket, http.server, socketserver, sys, os
+
+if os.name == 'posix' :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("0.0.0.254", 80))
+    ip_address=s.getsockname()[0]
+else :
+    ip_address = socket.gethostbyname(socket.gethostname())
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving Current Directory at: {ip_address}:{PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print(f"EXITING")
+        httpd.server_close()
+        sys.exit(0)
+
